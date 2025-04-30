3a16e965fb4517f030593e4ffeb856c2473a6485
Jason Whitlow <jason.whitlow45@gmail.com>
2025-04-29 20:00:19 -0700

    add control state

--- countdown.py
+++ countdown.py
@@ -9,29 +9,35 @@ root.geometry('400x300')
 root.configure(bg='lightblue')
 
 # global var
-STARTING_TIME = 5
-time_left = STARTING_TIME
+prev_time_entry = 5
+time_left = prev_time_entry
 timer_job_id = None
 
-def update_timer_entry():
+# updates the correct time in the timer
+def update_timer_entry_display():
     global time_left
+    timer_entry.config(state="normal")
     timer_entry.delete(0, tk.END)
     timer_entry.insert(0, f"{time_left//60}:{time_left % 60:02d}")
+    timer_entry.config(state="readonly")
 
 #Create the timer label
-timer_entry = tk.Entry(root, font=(
-    "Helvetica",48), bg = "lightblue", justify = "center")
+timer_entry = tk.Entry(root, font=("Helvetica",48), bg = "lightblue", justify = "center")
 timer_entry.pack(pady=20)
-update_timer_entry()
+update_timer_entry_display()
+timer_entry.config(state="normal")
 
-
-def parse_time_entry()-> bool:
-    global time_left
-
+# validates user input from timer_entry widget and adjusts global time_left
+def update_time_left_from_input()-> bool:
+    global time_left, prev_time_entry
     entry = timer_entry.get()
+
     if is_valid_timer_string(entry):
         minutes, seconds = map(int, entry.strip().split(":"))
         time_left = minutes * 60 + seconds
+
+        # when reset button is presssed the previous time that was entered by user will be used
+        prev_time_entry = time_left
         return True
     
     return False
@@ -45,23 +51,28 @@ def update_timer():
     global time_left, timer_job_id
 
     if time_left > 0:
-        update_timer_entry()
+        update_timer_entry_display()
         time_left -= 1
         timer_job_id = root.after(1000, update_timer)
 
-    elif time_left <= 0:
-        timer_entry.delete(0, tk.END)
-        timer_entry.insert(0, "Times Up")
+    else:
         root.after_cancel(timer_job_id)
         timer_job_id = None
-        time_left = STARTING_TIME
+        
+        update_timer_entry_display()
+        timer_entry.config(state="normal")
+
+        # when restart event occurs use the last entry from the user
+        time_left = prev_time_entry
+
 
 def start_timer():
     global timer_job_id
     print('start_timer()', timer_job_id)
 
     if not timer_job_id:
-        parse_time_entry()
+        timer_entry.config(state="readonly")
+        update_time_left_from_input()
         update_timer()
 
 def stop_timer():
@@ -70,15 +81,16 @@ def stop_timer():
     if timer_job_id:
         root.after_cancel(timer_job_id)
         timer_job_id = None
+        timer_entry.config(state="normal")
 
 def restart_timer():
-    global timer_job_id, time_left
+    global timer_job_id, time_left, prev_time_entry
     if timer_job_id:
         root.after_cancel(timer_job_id)
         timer_job_id = None
     
-    time_left = STARTING_TIME
-    update_timer_entry()
+    time_left = prev_time_entry
+    update_timer_entry_display()
     start_timer()
 
 
